57b92e886bf59376f24517007194c84b07d19145
Maurice Barnett <mauralanbarn@gmail.com>
2020-04-21 00:28:24 -0400

    Yoshi Commits Tax Fraud

---
+++ client.py
@@ -0,0 +1,12 @@
+import sys
+import socket
+
+HOST = '10.10.1.2'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
