765a4a4114589ff874f5afc09dd80abc2b5329e0
avanscy <avanscy@pcvm2-6.instageni.illinois.edu>
2020-04-27 20:44:48 -0500

    Add power-specific input from user; transmit data structure

--- tclient.py
+++ tclient.py
@@ -4,18 +4,30 @@ import socket
 import time
 import random
 import sys
+import struct
 
 HOST = "10.10.1.2"
 PORT = 20202
 BUF_SIZE = 1024
 
-msg = input(">> ")
+def get_data():
+    v = float(input("Input Voltage: "))
+    a = float(input("Input Amperage: "))
+    f = float(input("Input Frequency: "))
+    t = float(input("Input Threshold: "))
+    p = float(input("Input Period: "))
+    return(struct.pack("!ddddd", v, a, f, t, p))
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-while not msg.startswith('exit'):
-    s.sendall(bytes(msg, encoding='utf-8'))
+cmd = str(input("Continue? Type 'exit' to quit: "))
+#while not msg.startswith('exit'):
+while not cmd.startswith('exit'):
+    msg = get_data()
+    #s.sendall(bytes(msg, encoding='utf-8'))
+    s.sendall(msg)
     data = s.recv(BUF_SIZE)
     decoded = str(data, encoding='utf-8')
     if decoded.startswith('exit'):
@@ -24,7 +36,7 @@ while not msg.startswith('exit'):
         sys.exit(1)
     else:
         print("[+] Received data", str(data, encoding='utf-8'))
-        msg = input(">> ")
+    cmd = str(input("Continue? Type 'exit' to quit: "))
     
 s.sendall(bytes("exit", encoding='utf-8'))
 s.close()
